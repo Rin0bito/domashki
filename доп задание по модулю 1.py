4c0dd7a4b15b9cb4422d2400879c7e046cef6c69
Rin0bito <oleg.malarov.1995@gmail.com>
2024-10-27 19:36:31 +0500

    Add files via upload

---
+++ доп задание по модулю 1.py
@@ -0,0 +1,11 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print(len(grades))
+grades[0] = (5 + 3 + 3 + 5 + 4) / len(grades[0])
+grades[1] = (2+2+2+3) / len(grades[1])
+grades[2] = (4+5+5+2) / len(grades[2])
+grades[3] = (4+4+3) / len(grades[3])
+grades[4] = (5+5+5+4+5) / len(grades[4])
+print(grades)
+GPA = {'Aaron': 4, 'Bilbo': 2.25, 'Johnny': 4, 'Khendrik': 3.6, 'Steve': 4.8}
+print(GPA)
